5798013172722ef3b6e2a676f113c6306d47d59f
Dan Walker <D.K.D.Walker@leeds.ac.uk>
2012-04-25 16:09:19 +0100

    initial protocol processing class

---
+++ python-test-scripts/protocol-class.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#Class to read CSV protocol descriptions and present them to Python
+import os, csv
+
+class DecadesDataProtocols():
+   location = "/opt/decades/"
+   protocols = {} #Dictionary of protocols
+   field_types_map = {'boolean':'boolean', 'signed_int':'integer', 'single_float':'real', 'double_float':'real', 'text':'varchar', 'unsigned_int':'int'} # maps CSV protocol file "types" to PostgreSQL field types (postgreSQL does not have unsigned values
+   
+   def __init__(self):
+      dirList=os.listdir(self.location)
+      for protocol_description in dirList:
+         self.protocols[protocol_description[0:-4]] = []
+         protocolReader = csv.DictReader(open(os.path.join(self.location,protocol_description), 'rb'))
+         for row in protocolReader:
+            self.protocols[protocol_description[0:-4]].append(row)
+
+   def create_table(self, protocol_description):
+      #returns a suitable (Postgres)SQL CREATE TABLE command for a named protocol
+      s = 'CREATE TABLE %s (' % protocol_description
+      for field in self.protocols[protocol_description]:
+         #created postgres field spec. Strips leading $ from field name as it won't work
+         s = s + " ".join([field['field'].lstrip('$'),self.field_types_map[field['type']],','])
+
+      s = s.rstrip(',') + ")"
+      return s
+         
+         
+
+ddp = DecadesDataProtocols()
+print ddp.create_table('GINDAT01')
+      
