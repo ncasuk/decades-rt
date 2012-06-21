e163f8c9b530fdf4185b04d6f4d97b1b4b8c6d1b
Dan Walker <D.K.D.Walker@leeds.ac.uk>
2012-06-21 10:36:57 +0100

    Works with nondeiced indicated air temp

--- pylib/database-simulator.py
+++ pylib/database-simulator.py
@@ -8,16 +8,18 @@ import psycopg2, datetime, time, math
 conn = psycopg2.connect (host = "localhost",
                            user = "inflight",
                            password = "wibble",
-                           database = "inflightdata_test")
+                           database = "inflightdata")
 
 #delete previous run
 cursor = conn.cursor()
-cursor.execute('TRUNCATE TABLE scratchdata;')
-cursor.execute('ALTER SEQUENCE scratchdata_id_seq RESTART WITH 1;')
+cursor.execute('TRUNCATE TABLE mergeddata;')
+cursor.execute('ALTER SEQUENCE mergeddata_id_seq RESTART WITH 1;')
 conn.commit()
 while 1:
-            time.sleep(3)
+            time.sleep(1)
             seconds_since_midnight = (datetime.datetime.now() - datetime.datetime(datetime.datetime.now().year,datetime.datetime.now().month,datetime.datetime.now().day)).seconds
-            cursor.execute('INSERT INTO scratchdata (time_from_midnight, deiced_true_airtemp) VALUES (' + str(seconds_since_midnight) + ', ' + str(274.1 + (5 * math.sin(seconds_since_midnight/3))) +');')
+            timestamp = int(math.floor(time.time()))
+            
+            cursor.execute('INSERT INTO mergeddata (utc_time, prtaft01_utc_time, corcon01_utc_time, uppbbr01_utc_time, gindat01_utc_time, lowbbr01_utc_time, aerack01_utc_time, corcon01_ndi_temp) VALUES (' + str(timestamp) + ', ' + str(timestamp) + ', ' + str(timestamp) + ', ' + str(timestamp) + ',' + str(timestamp) + ',' + str(timestamp) + ',' + str(timestamp) + ', ' + str(911223 + (1204 * math.sin(timestamp/3))) +');')
             conn.commit()
 
