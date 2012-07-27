426d5e0e389a57e0e8a24e5d7f02aea8289cfb6e
Dan Walker <D.K.D.Walker@leeds.ac.uk>
2012-07-27 15:16:49 +0100

    Undoing mistake!

--- debian/decades/usr/local/lib/decades/pylib/database.py
+++
@@ -1,9 +0,0 @@
-import psycopg2, psycopg2.extensions
-def get_database():
-    conn = psycopg2.connect (host = "localhost",
-                           user = "inflight",
-                           password = "wibble",
-                           database = "inflightdata")
-    #turn off transactions so the incoming INSERTS do not interfere with each other
-    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) 
-    return conn
