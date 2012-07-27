ada7d27cd9f58767f3ff487ab99a94aaf05d51b2
Dan Walker <D.K.D.Walker@leeds.ac.uk>
2012-07-27 17:08:06 +0100

    test output filename change

--- pylib/decades_tcp_listener.py
+++ pylib/decades_tcp_listener.py
@@ -2,7 +2,7 @@ from twisted.internet.protocol import Protocol
 
 class DecadesTCPListener(Protocol):
    def __init__(self):
-      self.outfile = open('3502.out','w')
+      self.outfile = open('/usr/local/lib/decades/3502.out','w')
 
    def dataReceived(self, data):
       self.outfile.write(data)
