5e770cd14da7ce0615ad0640c98a83e58081118e
Dan Walker <D.K.D.Walker@leeds.ac.uk>
2012-11-21 14:34:02 +0000

    Fixing nan filter!

--- pylib/rt_calcs/rt_derive.py
+++ pylib/rt_calcs/rt_derive.py
@@ -854,11 +854,10 @@ C ST    - Corrected Surface Temperature   (deg C)
         raw.append(self.getdata('aerack01_utc_time',data))
         raw.append(self.getdata('lowbbr01_utc_time',data)) 
         raw.append(self.getdata('uppbbr01_utc_time',data)) 
-        print raw
-        dummy = raw[0]
+        dummy = raw[0] # because if they are all NaN, it should return NaN
         if len(raw[0]) >0: #i.e. it isn't the dummy pass
             #filter out NaNs
-            raw = [x for x in raw if np.isnan(x[0])] 
+            raw = [x for x in raw if not np.isnan(x[0])] 
         unixtime_at_midnight = time.mktime(datetime.now().timetuple()[0:3]+(0,0,0,0,0,0))
         #raw is an array, so subtracting an integer appears to be valid
         if len(raw) >0:
