279006d3a9c7580a6aecaf6bce3418244d67fafe
Dan Walker <D.K.D.Walker@leeds.ac.uk>
2012-11-22 16:01:55 +0000

    experimental split TCP incoming filenames by DLU

--- pylib/decades_tcp_listener.py
+++ pylib/decades_tcp_listener.py
@@ -1,9 +1,10 @@
 from twisted.internet.protocol import Protocol
+from twisted.python import log
 from datetime import datetime
+from decades import DecadesDataProtocols as ddp
 
 class DecadesTCPListener(Protocol):
-   def __init__(self):
-      self.outfile = open('/opt/decades/output/decades-tcp-' + datetime.utcnow().strftime('%Y-%m-%d_%H.%M.%S') +'.bin','w')
-
    def dataReceived(self, data):
+      log.msg('TCP data from ' data[1:8])
+      self.outfile = open('/opt/decades/output/decades-tcp-' + data[1:8] +'-'+datetime.utcnow().strftime('%Y-%m-%d_%H.%M.%S') +'.bin','w')
       self.outfile.write(data)
