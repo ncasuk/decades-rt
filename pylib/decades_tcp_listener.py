394be1c9c4e69226ee67090d1f312580320ebd1d
Dan Walker <D.K.D.Walker@leeds.ac.uk>
2012-11-22 16:29:39 +0000

    Separet files, but one per instance

--- pylib/decades_tcp_listener.py
+++ pylib/decades_tcp_listener.py
@@ -1,10 +1,15 @@
 from twisted.internet.protocol import Protocol
 from twisted.python import log
 from datetime import datetime
-from decades import DecadesDataProtocols as ddp
+from decades import DecadesDataProtocols
 
 class DecadesTCPListener(Protocol):
+   dataProtocols = DecadesDataProtocols() 
+   outfiles = []
+   def __init__(self):
+      for each in self.dataProtocols.available():
+         self.outfiles[each] = open('/opt/decades/output/decades-tcp-' + each +'-'+datetime.utcnow().strftime('%Y-%m-%d_%H.%M.%S') +'.bin','w')
+            
    def dataReceived(self, data):
-      log.msg('TCP data from ' + data[1:8])
-      self.outfile = open('/opt/decades/output/decades-tcp-' + data[1:8] +'-'+datetime.utcnow().strftime('%Y-%m-%d_%H.%M.%S') +'.bin','w')
-      self.outfile.write(data)
+      log.msg('TCP data from ' + data[1:9])
+      self.outfiles[data[1:9]].write(data)
