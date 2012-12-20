d8701c7ed9bddde23efe6d5be13ac9c7b6a6907b
Dan Walker <D.K.D.Walker@leeds.ac.uk>
2012-12-20 14:43:51 +0000

    First go at new TCP filenames

--- pylib/decades_tcp_listener.py
+++ pylib/decades_tcp_listener.py
@@ -8,8 +8,17 @@ class DecadesTCPListener(Protocol):
    outfiles = {} #dictonary of output files.
    def __init__(self):
       for each in self.dataProtocols.available():
-         self.outfiles[each] = open('/opt/decades/output/decades-tcp-' + each +'-'+datetime.utcnow().strftime('%Y-%m-%d_%H.%M.%S') +'.bin','w')
+         self.outfiles[each] = {} #dictionary per instrument for fligh #s
             
    def dataReceived(self, data):
-      log.msg('TCP data from ' + data[1:9])
-      self.outfiles[data[1:9]].write(data)
+      instrument=data[1:9] #e.g PRTAFT01
+      flightno=data[20:24] # e.g. XXXX, SIMU, B751
+      log.msg('TCP data from ' + instrument + ' ' + flightno)
+      self.writedata(data, instrument, flightno)
+   
+   def writedata(self, data, instrument, flightno):
+      try:
+         self.outfiles[instrument][flightno].write(data)
+      except KeyError:
+         self.outfiles[instrument][flightno] = open('/opt/decades/output/' + instrument +'_'+datetime.utcnow().strftime('%Y%m%d_%H%M%S') +'_' + flightno,'w')
+
