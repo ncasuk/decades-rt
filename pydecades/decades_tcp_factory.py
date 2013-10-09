2aa16926fc27e61b5c01eacdbfaaf9f4a47ab4d4
Dan Walker <D.K.D.Walker@leeds.ac.uk>
2013-10-09 15:33:40 +0100

    Shifting the outfile management Protocol->Factory

--- pydecades/decades_tcp_factory.py
+++ pydecades/decades_tcp_factory.py
@@ -6,10 +6,21 @@ from twisted.python import log
 from sys import stdout
 
 from decades_tcp_listener import DecadesTCPListener
+from decades import DecadesDataProtocols
 
 class DecadesTCPFactory(Factory):
+    protocol = DecadesTCPListener
+    dataProtocols = DecadesDataProtocols() 
+    outfiles = {} 
+
+    def __init__(self):
+      for each in self.dataProtocols.available():
+         self.outfiles[each] = {} #dictionary per instrument for fligh #s
+
     def buildProtocol(self, addr):
-        return DecadesTCPListener()
+      d = Factory.buildProtocol(self, addr)
+      d.factory = self
+      return d
 
 def main():# Listen for TCP:3502
    log.startLogging(stdout)
--- pydecades/decades_tcp_listener.py
+++ pydecades/decades_tcp_listener.py
@@ -1,15 +1,13 @@
 from twisted.internet.protocol import Protocol
 from twisted.python import log
 from datetime import datetime
-from decades import DecadesDataProtocols
 import os
 from distutils.dir_util import mkpath
 
 from ConfigParser import SafeConfigParser
 
 class DecadesTCPListener(Protocol):
-   dataProtocols = DecadesDataProtocols() 
-   outfiles = {} #dictonary of output files.
+   #outfiles = {} #dictonary of output files.
    parser = SafeConfigParser()
    config = parser.read(['/etc/decades/decades.ini','decades.ini'])
    
@@ -17,8 +15,7 @@ class DecadesTCPListener(Protocol):
       self.output_dir = self.parser.get('Config','output_dir')
       #interprets the mode as an octal int
       self.output_create_mode = int(self.parser.get('Config','output_create_mode'),8)
-      for each in self.dataProtocols.available():
-         self.outfiles[each] = {} #dictionary per instrument for fligh #s
+      log.msg('Initialising protocol')
             
    def dataReceived(self, data):
       instrument=data[1:9] #e.g PRTAFT01
@@ -28,17 +25,17 @@ class DecadesTCPListener(Protocol):
    
    def writedata(self, data, instrument, flightno):
       try:
-         self.outfiles[instrument][flightno].write(data)
+         self.factory.outfiles[instrument][flightno].write(data)
       except KeyError: #i.e.file does not exist yet
          try: #try to create file 
             os.umask(0)
             dt = datetime.utcnow()
             outpath = os.path.join(self.output_dir,dt.strftime('%Y'), dt.strftime('%m'))
-            mkpath(outpath, mode=self.output_create_mode + 0111) #acts like "mkdir -p" so if exists returns a success (+0111 adds executable bit as they ar dirs)
+            mkpath(outpath, mode=self.output_create_mode + 0111) #acts like "mkdir -p" so if exists returns a success (+0111 adds executable bit as they are dirs)
 
             outfile = os.path.join(outpath,instrument + '_'+dt.strftime('%Y%m%d_%H%M%S') +'_' + flightno)
             log.msg('Creating output file ' + outfile)
-            self.outfiles[instrument][flightno] = os.fdopen(os.open(outfile, os.O_WRONLY | os.O_CREAT, self.output_create_mode), 'w')
+            self.factory.outfiles[instrument][flightno] = os.fdopen(os.open(outfile, os.O_WRONLY | os.O_CREAT, self.output_create_mode), 'w')
          except TypeError: 
             '''usually some incoming data corruption so 'instrument' and/or 'flightno'
             are not valid due to containing some NULL bytes; ignore data in that case'''
