615f9568dbec0beac781c0e08ff58ea1e841997a
Dan Walker <D.K.D.Walker@leeds.ac.uk>
2013-10-16 12:23:56 +0100

    Shifting DB config to main config file

--- pydecades/database.py
+++ pydecades/database.py
@@ -1,9 +1,12 @@
 import psycopg2, psycopg2.extensions
+from pydecades.configparser import DecadesConfigParser
+
 def get_database():
-    conn = psycopg2.connect (host = "localhost",
-                           user = "inflight",
-                           password = "wibble",
-                           database = "inflightdata")
+    parser = DecadesConfigParser()
+    conn = psycopg2.connect (host = parser.get('Database','host'),
+                           user = parser.get('Database','user'),
+                           password = parser.get('Database','password'),
+                           database = parser.get('Database','database'))
     #turn off transactions so the incoming INSERTS do not interfere with each other
     conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) 
     return conn
