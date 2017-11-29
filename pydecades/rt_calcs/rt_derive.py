c95e234b2216e6ae25b35678a060893916487e43
Dan Walker <daniel.walker@ncas.ac.uk>
2017-11-29 12:22:19 +0000

    septic sync - vp2dp forumla calls

--- pydecades/rt_calcs/rt_derive.py
+++ pydecades/rt_calcs/rt_derive.py
@@ -7,47 +7,6 @@ import time
 import formulas
 from twisted.python import log
 
-def vp2dp_buck(vp, p, temp):
-    """Water vapour to dew point conversion using the formula from the 
-    Buck CR2 hygrometer manual p19f. Returns the dew point in K.
-    
-    Input:
-      vp: vapour pressure in mb
-      p: air pressure in mbar
-      temp: air temperature in Kelvin
-
-    Output:
-      dew point temperature in K
-    
-    """
-    a, b, c, d = (6.1121, 18.678, 257.14, 234.5)
-    ef=1.0+10**-4*(2.2+p/10.*(0.0383+6.4*10**-5*(temp-273.15)*2))
-    s=np.log(vp/ef)-np.log(a)
-    result=d/2.0 * (b-s-((b-s)**2-4*c*s/d)**0.5)
-    return result+273.15
-
-
-def vp2fp_buck(vp, p, temp):
-    """Water vapour to frost point conversion using the formula from the 
-    Buck CR2 hygrometer manual p19f. Returns the dew point in K.
-    
-    Input:
-      vp: vapour pressure in mb
-      p: air pressure in mbar
-      temp: air temperature in Kelvin
-
-    Output:
-      frost point temperature in K
-    
-    """
-    a, b, c, d=(6.1115, 23.036, 279.82, 333.7)
-    ef=1.0+10**-4*(2.2+p/10.*(0.0383+6.4*10**-5*(temp-273.15)*2))
-    s=np.log(vp/ef)-np.log(a)
-    result=d/2.0 * (b-s-((b-s)**2-4*c*s/d)**0.5)
-    return result+273.15
-
-
-
 class derived(rt_data.rt_data):
     """ A collection of the processing routines for realtime in flight data """
     def pressure_height_feet(self,data):
@@ -1084,7 +1043,7 @@ C ST    - Corrected Surface Temperature   (deg C)
     
         wmr=vmr/1.6077
         vp=wmr*p/(622*10**3+wmr)
-        dp=vp2dp_buck(vp, p, temp)
+        dp=formulas.vp2dp(vp, p, temp)
         return dp-273.15
     
     
@@ -1096,7 +1055,7 @@ C ST    - Corrected Surface Temperature   (deg C)
     
         wmr=vmr/1.6077
         vp=wmr*p/(622*10**3+wmr)
-        dp=vp2dp_buck(vp, p, temp)
+        dp=formulas.vp2dp(vp, p, temp)
         return dp-273.15
 
     def seaprobe_ice_water_83(self,data):
